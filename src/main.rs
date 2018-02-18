65db6cb98a971b4de87bf0747f5d5b30fa2e296d
OKUNOKENTARO <okunokentaro@me.com>
2018-02-18 18:55:31 +0900

    implement docopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Usage:
+  tser (-h | --help)
+  tser (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_version: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
