614a8a9a540e692d073e6e9299e0a2b49208e0c8
OKUNOKENTARO <okunokentaro@me.com>
2018-02-19 09:43:28 +0900

    add arg_path

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 extern crate serde_derive;
 extern crate docopt;
 
+use std::fs;
 use docopt::Docopt;
 
 const USAGE: &'static str = "
 Usage:
+  tser <path>
   tser (-h | --help)
   tser (-v | --version)
 
@@ -16,6 +18,7 @@ Options:
 
 #[derive(Debug, Deserialize)]
 struct Args {
+    arg_path: Vec<String>,
     flag_version: bool,
 }
 
@@ -23,5 +26,14 @@ fn main() {
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
-    println!("{:?}", args);
+    println!("{:?}", args.arg_path);
+
+    let paths = fs::read_dir("./src").unwrap();
+
+    for path in paths {
+        let arg_path_str = args.arg_path.iter().cloned().collect::<String>();
+        let path_str = path.unwrap().path().display().to_string();
+        println!("Name: {}", path_str);
+        println!("Name: {}", arg_path_str == path_str);
+    }
 }
