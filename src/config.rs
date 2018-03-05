8325155cc059928cdf21441483fe17be6d4aac1d
OKUNOKENTARO <okunokentaro@me.com>
2018-03-05 10:18:12 +0900

    declare type Result

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
 extern crate toml;
-use std::{env, path};
+use std::{env, path, result};
 use reader::read_file;
+
+type Result<T> = result::Result<T, String>;
 
 #[derive(Debug, Deserialize)]
 pub struct Rule {
@@ -15,7 +17,7 @@ pub struct Config {
     pub rules: Option<Vec<Rule>>,
 }
 
-fn read_config(mut dir: path::PathBuf) -> Result<Config, String> {
+fn read_config(mut dir: path::PathBuf) -> Result<Config> {
     let config_file_path = dir.join("config.toml");
     match read_file(&config_file_path) {
         Ok(val) => toml::from_str(&val).map_err(|e| e.to_string()),
@@ -26,7 +28,7 @@ fn read_config(mut dir: path::PathBuf) -> Result<Config, String> {
     }
 }
 
-pub fn get_config() -> Result<Config, String> {
+pub fn get_config() -> Result<Config> {
     env::current_dir()
         .map_err(|e| e.to_string())
         .and_then(|pwd| {
