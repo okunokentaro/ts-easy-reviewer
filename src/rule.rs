2af613fa03f88a49addf2e9f6973cde6a7b96a45
OKUNOKENTARO <okunokentaro@me.com>
2018-03-05 17:14:27 +0900

    WIP implement RuleType

--- src/rule.rs
+++ src/rule.rs
@@ -2,50 +2,65 @@ use regex::Regex;
 use config;
 
 #[derive(Debug)]
+pub struct RuleType {
+    rule_type: Option<String>,
+    context: Option<String>,
+}
+
+impl RuleType {
+    pub fn new() -> RuleType {
+        RuleType {rule_type: None, context: None}
+    }
+
+    pub fn set_rule_type(&mut self, v: &str) {
+        match v {
+            "class_implements" => {
+                self.rule_type = Some("class_implements".to_string());
+            },
+            "class_extends" => {
+                self.rule_type = Some("class_extends".to_string());
+            },
+            "method_returns" => {
+                self.rule_type = Some("method_returns".to_string());
+            },
+            _ => {}
+        }
+    }
+
+    pub fn set_context(&mut self, v: &str) {
+        self.context = Some(v.to_string());
+    }
+}
+
+#[derive(Debug)]
 pub struct Rule {
     name: String,
     statement: String,
+    rule_type: RuleType,
 }
 
 impl Rule {
-    pub fn check(&self, code: &String) {
-        println!("=================================================================");
-        println!("check {}", code);
-        println!("statement {}", self.statement);
-
-        let is_class_implements = self.statement.starts_with("class_implements");
-        if is_class_implements {
-            let re = Regex::new(r"class_implements\s'(.*?)'").unwrap();
-            let cap = re.captures(&self.statement).unwrap();
-            let implements = &cap[1];
-            println!("{}", implements);
-
-            let implements_re = Regex::new(&["implements", implements].join(" ")).unwrap();
-            let result = implements_re.is_match(code);
-            println!("{}", result);
-
-            let and_re = Regex::new(r"\sand\s").unwrap();
-            let and_result = and_re.is_match(&self.statement);
-            println!("{}", and_result);
-
-            let includes_re = Regex::new(r"includes\s'(.*?)'").unwrap();
-            let includes_re_cap = includes_re.captures(&self.statement).unwrap();
-            let includes = &includes_re_cap[1];
-            println!("{}", includes);
-
-            let includes_re_2 = Regex::new(includes).unwrap();
-            let result_2 = includes_re_2.is_match(code);
-            println!("{}", result_2);
-
-            if result_2 {
-                let then_error_re = Regex::new(r"\sthen\serror").unwrap();
-                let then_error_result = then_error_re.is_match(&self.statement);
-                println!("then_error_result {}", then_error_result);
-                if then_error_result {
-                    println!("error!!!!");
-                }
+    pub fn new(name: String, statement: String) -> Rule {
+        let cloned_statement = statement.clone();
+        let tokens = cloned_statement.split(" ");
+        let rule_type = tokens.enumerate().fold(RuleType::new(), |mut acc: RuleType, (i, v)| {
+            match i {
+                0 => {
+                    acc.set_rule_type(v);
+                    acc
+                },
+                1 => {
+                    acc.set_context(v);
+                    acc
+                },
+                _ => acc
             }
-        }
+        });
+        Rule {name, statement, rule_type}
+    }
+
+    pub fn check(&self, code: &String) {
+
     }
 }
 
@@ -54,9 +69,8 @@ pub fn get_rules(config: config::Config) -> Vec<Rule> {
 
     toml_rules
         .into_iter()
-        .map(|toml_rule| Rule {
-            name: toml_rule.name.unwrap(),
-            statement: toml_rule.rule.unwrap(),
+        .map(|toml_rule| {
+            Rule::new(toml_rule.name.unwrap(), toml_rule.rule.unwrap())
         })
         .collect()
 }
